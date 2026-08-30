91e63e83ee1b5ddbbc877c19adfc104aec296f64
Özgür Kesim <oec@codeblau.de>
2026-08-30 16:33:32 +0200

    [flake.nix] updated vendorHash

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
 
           # Set to lib.fakeHash first, then replace with the real value
           # from the build error output
-          vendorHash = sha256-YemCe8OrPdx4y7308ZlYmqqT7QdXrGz7WUX9BOkzBS8=;
+          vendorHash = "sha256-YemCe8OrPdx4y7308ZlYmqqT7QdXrGz7WUX9BOkzBS8=";
 
           doCheck = false;
 
